ce3939190420dbb96f4b6b5bb6fb3c970eb1af68
Dai Kawahara <skbsporta7@gmail.com>
2019-11-07 22:16:08 +0900

    add request api & handle api response

--- RxSwift-NewsApp/NewsTableViewController.swift
+++ RxSwift-NewsApp/NewsTableViewController.swift
@@ -15,9 +15,30 @@ let apiKey = RxSwiftNewsAppKeys().apiKey
 
 class NewsTableViewController: UITableViewController {
 
+    private var disposeBag = DisposeBag()
+    private var articles = [Article]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        populateNews()
+    }
 
+    func populateNews() {
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=\(apiKey)")!
+        Observable.just(url)
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 5.0)
+                return URLSession.shared.rx.data(request: request)
+        }.map { (data) -> [Article] in
+            return (try? JSONDecoder().decode(APIResponse.self, from: data).articles) ?? []
+        }.subscribe(onNext: { [weak self] articles in
+            // dataSource更新
+            self?.articles = articles
+            DispatchQueue.main.async {
+                print(articles)
+//                self?.tableView.reloadData()
+            }
+        }).disposed(by: disposeBag)
     }
 
     // MARK: - Table view data source
